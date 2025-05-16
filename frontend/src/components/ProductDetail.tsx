1238d8dbc876fa825a84662b59167ba06004dbe9
gittjar <jarno.keranen@gmail.com>
2025-05-16 19:17:55 +0300

    dont show edit or delete buttons if user isnt logged in

--- frontend/src/components/ProductDetail.tsx
+++ frontend/src/components/ProductDetail.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { getProductById } from '../services/api';
-import { deleteProduct } from '../services/api';
+import { getProductById, deleteProduct } from '../services/api';
 import ConfirmationDeleteProduct from '../modals/ConfirmationDeleteProduct';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import FeedbackModal from '../modals/FeedbackModal';
 
 interface Product {
   _id: string;
@@ -32,14 +30,17 @@ const ProductDetail: React.FC = () => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
 
-  const placeholderImage = 'https://placehold.co/600x400/white/gray?text=No+Image+uploaded'; // Placeholder for broken images
+  const placeholderImage = 'https://placehold.co/600x400/white/gray?text=No+Image+uploaded';
+
+  // Check if the user is authenticated
+  const isAuthenticated = !!localStorage.getItem('token');
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
         const productResponse = await getProductById(id ?? '');
         setProduct(productResponse.data);
-        setSelectedImage(productResponse.data.images[0] || placeholderImage); // Set the first image or placeholder
+        setSelectedImage(productResponse.data.images[0] || placeholderImage);
       } catch (error) {
         console.error('There was an error fetching the product!', error);
       }
@@ -58,100 +59,63 @@ const ProductDetail: React.FC = () => {
 
   const handleConfirmDelete = async () => {
     if (!product) return;
-  
+
     try {
-      await deleteProduct(product._id); // Call the delete API
+      await deleteProduct(product._id);
       console.log(`Product ${product.name} deleted successfully`);
       setShowDeleteModal(false);
-      navigate('/products'); // Redirect to the products list page
+      navigate('/products');
     } catch (error) {
       console.error('Error deleting the product:', error);
       alert('Failed to delete the product. Please try again.');
     }
   };
 
-  const handleCancelDelete = () => {
-    setShowDeleteModal(false);
-  };
-  const handleCloseFeedback = () => {
-    setShowDeleteModal(false);
-  };
   const handleClose = () => {
     setShowDeleteModal(false);
   };
 
-
-
-
-
   return (
     <div className="container d-flex justify-content-center align-items-center mb-4" style={{ marginTop: '100px' }}>
-    <div className={`card ${product.varastossa && product.quantity > 0 ? 'border-success' : 'border-danger'} rounded`} style={{ maxWidth: '600px', width: '100%' }}>
-      {selectedImage ? (
-        <div style={{ height: '400px', overflow: 'hidden' }}>
-          <img
-            src={selectedImage}
-            className="card-img-top"
-            alt={product.name}
-            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-            onError={(event) => {
-              event.currentTarget.src = placeholderImage; // Replace main image with placeholder
-            }}
-          />
-        </div>
-      ) : (
-        <div
-          className="d-flex justify-content-center align-items-center"
-          style={{
-            height: '400px',
-            backgroundColor: '#f8f9fa',
-            color: '#6c757d',
-            fontSize: '18px',
-            fontStyle: 'italic',
-          }}
-        >
-          No image available
-        </div>
-      )}
-      <div className="card-body" style={{ padding: '0' }}>
-        <h5 className="card-title mb-4 mt-4 text-center" style={{ margin: '10px 0' }}>{product.name}</h5>
-        {product.images.filter((image) => image.trim() !== '').length > 0 ? (
-          <div className="d-flex flex-wrap justify-content-center mb-2">
-            {product.images
-              .filter((image) => image.trim() !== '') // Filter out empty strings
-              .map((image, index) => (
-                <img
-                  key={index}
-                  src={image}
-                  alt={`${product.name} ${index + 1}`}
-                  className={`img-thumbnail ${selectedImage === image ? 'border-primary' : ''}`}
-                  style={{
-                    width: '80px',
-                    height: '80px',
-                    cursor: 'pointer',
-                    objectFit: 'cover',
-                    margin: '2px',
-                  }}
-                  onClick={() => setSelectedImage(image)}
-                  onError={(event) => {
-                    event.currentTarget.style.display = 'none'; // Hide broken thumbnail
-                  }}
-                />
-              ))}
+      <div className={`card ${product.varastossa && product.quantity > 0 ? 'border-success' : 'border-danger'} rounded`} style={{ maxWidth: '600px', width: '100%' }}>
+        {selectedImage ? (
+          <div style={{ height: '400px', overflow: 'hidden' }}>
+            <img
+              src={selectedImage}
+              className="card-img-top"
+              alt={product.name}
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              onError={(event) => {
+                event.currentTarget.src = placeholderImage;
+              }}
+            />
           </div>
         ) : (
-          <p className="text-muted text-center rounded border border-danger m-2 p-2">No images available, please update product and add images.</p>
+          <div
+            className="d-flex justify-content-center align-items-center"
+            style={{
+              height: '400px',
+              backgroundColor: '#f8f9fa',
+              color: '#6c757d',
+              fontSize: '18px',
+              fontStyle: 'italic',
+            }}
+          >
+            No image available
+          </div>
         )}
-        <p className="card-text mb-1 border-bottom" style={{ margin: '0 10px' }}>
-          <strong>Manufacturer:</strong> {product.manufacturer?.name || 'Unknown'}
-        </p>
-        <p className="card-text mb-1 list-group-item" style={{ margin: '0 10px' }}>
-          <strong>Category:</strong> {product.category}
-        </p>
-        <p className="card-text mb-1" style={{ margin: '0 10px' }}>
-          <strong>Price:</strong> ${product.price}
-        </p>
-        <p className="card-text mb-1" style={{ margin: '0 10px' }}>
+        <div className="card-body" style={{ padding: '0' }}>
+          <h5 className="card-title mb-4 mt-4 text-center" style={{ margin: '10px 0' }}>{product.name}</h5>
+          <p className="card-text mb-1 border-bottom" style={{ margin: '0 10px' }}>
+            <strong>Manufacturer:</strong> {product.manufacturer?.name || 'Unknown'}
+          </p>
+          <p className="card-text mb-1" style={{ margin: '0 10px' }}>
+            <strong>Category:</strong> {product.category}
+          </p>
+          <p className="card-text mb-1" style={{ margin: '0 10px' }}>
+            <strong>Price:</strong> ${product.price}
+          </p>
+          <p className="card-text mb-1" style={{ margin: '0 10px' }}>
           <strong>Description:</strong> {product.description || 'No description available'}
         </p>
         <p className="card-text mb-1" style={{ margin: '0 10px' }}>
@@ -166,33 +130,36 @@ const ProductDetail: React.FC = () => {
         <p className="card-text mb-1" style={{ margin: '0 10px' }}>
           <strong>Quantity:</strong> {product.quantity}
         </p>
-      </div>
-      <div className="card-footer" style={{ padding: '0', textAlign: 'center' }}>
-        <small className="text-body-secondary" style={{ display: 'block', margin: '10px 0' }}>
-          Last Updated: {new Date(product.updated_at).toLocaleString()}
-        </small>
-      </div>
-      <div className="d-flex justify-content-center mb-2">
-        <button
-          className="btn btn-primary mt-2 mx-2"
-          onClick={() => navigate(`/edit-product/${product._id}`)}
-        >
-          Edit Product
-        </button>
-        <button
-          className="btn btn-secondary mt-2 mx-2"
-          onClick={() => window.history.back()}
-        >
-          Back to Product List
-        </button>
-
-        <button
-          className="btn btn-danger mt-2 mx-2"
-          onClick={handleDelete}
-        >
-          Delete Product
-        </button>
-
+        </div>
+        <div className="card-footer" style={{ padding: '0', textAlign: 'center' }}>
+          <small className="text-body-secondary" style={{ display: 'block', margin: '10px 0' }}>
+            Last Updated: {new Date(product.updated_at).toLocaleString()}
+          </small>
+        </div>
+        <div className="d-flex justify-content-center mb-2">
+          {isAuthenticated && (
+            <>
+              <button
+                className="btn btn-primary mt-2 mx-2"
+                onClick={() => navigate(`/edit-product/${product._id}`)}
+              >
+                Edit Product
+              </button>
+              <button
+                className="btn btn-danger mt-2 mx-2"
+                onClick={handleDelete}
+              >
+                Delete Product
+              </button>
+            </>
+          )}
+          <button
+            className="btn btn-secondary mt-2 mx-2"
+            onClick={() => window.history.back()}
+          >
+            Back to Product List
+          </button>
+        </div>
         <ConfirmationDeleteProduct
           show={showDeleteModal}
           onClose={handleClose}
@@ -201,7 +168,6 @@ const ProductDetail: React.FC = () => {
         />
       </div>
     </div>
-  </div>
   );
 };
 
