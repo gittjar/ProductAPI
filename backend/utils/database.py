4ffa79599491fcbf50a07896924ac6cde5ce3256
Jarno K <jarno.keranen@gmail.com>
2026-02-28 20:52:16 +0200

    config deployment

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from backend.config import Config
+from config import Config
 
 # Use connect=False to delay connection until first operation
 # Simple connection without custom SSL context - let pymongo handle it
